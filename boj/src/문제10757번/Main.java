bc05e0cc9f0be531b828064a8e11c5a964a5139e
jyh <jyh9177@naver.com>
2021-12-22 19:33:18 +0900

    문제10757번

---
+++ boj/src/문제10757번/Main.java
@@ -0,0 +1,17 @@
+package 문제10757번;
+import java.util.Scanner;
+import java.math.BigInteger;
+ 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		BigInteger A = new BigInteger(in.next());
+		BigInteger B = new BigInteger(in.next());
+		
+		A = A.add(B);
+		
+		System.out.println(A.toString());
+	}
+}
